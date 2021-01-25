92df5a68c9e497bd04784c2ee9b65942a78f2d6d
Mark Kenneth Ranosa <ken.ranosa@gmail.com>
2021-01-25 15:22:56 +0800

    Initial implementation

--- dine-in.spec.ts
+++ dine-in.spec.ts
@@ -3,8 +3,59 @@ import { config } from 'folio';
 
 config.timeout = 60000;
 
-it("is a basic test with the page", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-  const name = await page.innerText(".navbar__title");
-  expect(name).toBe("Playwright");
+it("flow: dine in", async ({ page }) => {
+  // should be able to load demo page
+  await page.goto('https://staging.mryum.com/demo');
+  expect(page.url()).toEqual('https://staging.mryum.com/demo');
+
+  // should be able to go to to dine in page
+  await page.click('text="Table Service"');
+  expect(page.url()).toEqual('https://staging.mryum.com/demo/dine-in');
+
+  // should be able to add table number
+  await page.click('input[name="tableNumber"]');
+  await page.fill('input[name="tableNumber"]', '33');
+  await page.click('text="Confirm"');
+
+  // should be able to go to drinks page
+  await page.click('text="Drinks 🍻"');
+  expect(page.url()).toEqual('https://staging.mryum.com/demo/dine-in/drinks');
+
+  // should be able to go close drinks page notification
+  await page.click('text="Thanks!"');
+
+  // should be able to add drinks to cart
+  await page.click('text="Left Hand Negroni"');
+  await page.click('text="Add to cart"');
+
+  await page.click('text="Latte"');
+  await page.click('text="Skim Milk"');
+  await page.click('text="1 Sugar"');
+  await page.click('text="Sweet Potato Chips"');
+  await page.click('text="Add to cart"');
+
+  await page.click('text="House Rose"');
+  await page.click('text="Add to cart"');
+  
+  await page.click('text="Pinot Gri"');
+  await page.click('text="Bottle"');
+  await page.click('text="Add to cart"');
+
+  // should be able to go to checkout page
+  await page.click('text="Cart"');
+
+  // TODO should have the correct added drinks
+  
+  // should be able to checkout
+  await page.click('//div[normalize-space(.)=\'CHECKOUT FOR TABLE 33\']');
+  await page.click('text="Maybe next time"');
+  expect(page.url()).toEqual('https://staging.mryum.com/demo/checkout/dine-in/cart');
+
+  await page.fill('input[aria-label="Please enter a phone number without the country dial code."]', '400000000');
+  await page.click('text="SEND SMS CODE"');
+  await page.fill('input[aria-label="Please enter your pin code"]', '3066');
+
+  await page.click('text="PAY NOW"');
+
+  await page.waitForTimeout(5000);
 });
